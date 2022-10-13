6041edc26d608431caee1009e37475290f06d72a
stronk-exe <ael-asri@student.1337.ma>
2022-10-13 20:42:52 +0100

    constracting vector

--- ft_vector/ft_vector.hpp
+++ ft_vector/ft_vector.hpp
@@ -17,40 +17,45 @@
 
 namespace ft
 {
-	template <class T, class Allocator = std::allocator<T> > class ft_vector
+	template <class T, class Allocator = std::allocator<T> > class vector
 	{
+		// definin
+		typedef T				value_type;
+		typedef Allocator		allocator_type;
+		typedef size_t			size_type;
+		typedef	vector<T>::iterator		iterator;
+
 		private:
-			int	n;
-			T	*v;
+			size_type	n;
+			T			*v;
 		public:
 			// Member functions
 				// -- Constructors
-					explicit ft_vector ( const T& alloc = T() )	
+					// explicit ft_vector ( const T& alloc = T() )
+					explicit vector (const allocator_type& alloc = allocator_type())
 					{
 						n = 0;
 						(void)alloc;
 						v = new T[n];
 						// v = alloc.allocate(n);
-						std::cout << "ft_vector default constractor called!" << std::endl;
+						std::cout << "vector default constractor called!" << std::endl;
 					};	// default constactor
-					explicit ft_vector (
-						int n, const T& val = T(), 
-						const T& alloc = T())
+					// explicit ft_vector (int n, const T& val = T(), const T& alloc = T())					
+					explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
 					{
 						this->n = n;
 						(void)alloc;
 						//  v = alloc.allocate(n);
 						v = new T[n];
-						for (int i=0; i<n; i++)
+						for (size_type i=0; i<n; i++)
 						{
 							v[i] = val;
 							std::cout << v[i] << std::endl;
 						}
-						std::cout << "ft_vector fill constractor called!" << std::endl;
+						std::cout << "vector fill constractor called!" << std::endl;
 					};	// fill constractor
-			/*		template <class InputIterator> ft_vector (
-						InputIterator first, InputIterator last,
-						const T& alloc = T())
+					// template <class InputIterator> ft_vector (InputIterator first, InputIterator last, const T& alloc = T())
+			/*		template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
 					{
 						int i=0;
 						while (first != last)
@@ -60,30 +65,42 @@ namespace ft
 							i++;
 						}
 						std::cout << "vector range constractor called!" << std::endl;
-					};	// range constractor
-			*/		ft_vector (const ft_vector& v)
+					};*/	// range constractor
+					// ft_vector (const ft_vector& v)
+					vector (const vector& x)
 					{
-						*this = v;
-						for (int i=0; i<this->n; i++)
+						*this = x;
+						for (size_type i=0; i<this->n; i++)
 						{
 							std::cout << this->v[i] << std::endl;
 						}
-						std::cout << "ft_vector copy constractor called!" << std::endl;
+						std::cout << "vector copy constractor called!" << std::endl;
 					};					// copy constractor
 
 				// -- Deconstructors
-					~ft_vector()
+					// ~ft_vector()
+					~vector()
 					{
-						std::cout << "ft_vector Deconstractor called!" << std::endl;
-					};		/*										// deconstractor
-
+						std::cout << "vector Deconstractor called!" << std::endl;
+					};												// deconstractor
+/*
 				// -- Operator=
-					vector& operator= (const vector& x);	*/				// copy assignement operator
+					vector& operator= (const vector& x);					// copy assignement operator
 
-	/*		// Iterators
-			begin
-			end
-			rbegin
+	*/		// Iterators
+			// begin
+			iterator begin()
+			{
+				return v;
+			};
+			// end
+			iterator end()
+			{
+				while (*v)
+					*(v)++;
+				return v;
+			}
+	/*		rbegin
 			rend
 			cbegin
 			cend
--- ft_vector/main.cpp
+++ ft_vector/main.cpp
@@ -4,18 +4,22 @@
 
 int main ()
 {
+	/*		constractors, deconstractor, copy assignment and iterators		*/
+
 	// constructors used in the same order as described above:
-	ft::ft_vector<int> first;                                // empty vector of ints
-	ft::ft_vector<int> second (4,100);                       // four ints with value 100
+	ft::vector<int> first;                                // empty vector of ints
+	ft::vector<int> second (4,100);                       // four ints with value 100
 	// ft::ft_vector<int> third (second.begin(),second.end());  // iterating through second
 	// ft::ft_vector<int> fourth (third);                       // a copy of third
-	ft::ft_vector<int> fourth (second); 
+	ft::vector<int> fourth (second); 
 	// // the iterator constructor can also be used to construct from arrays:
-	// int myints[] = {16,2,77,29};
-	// ft::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
-	// std::cout << "The contents of fifth are:";
-	// for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
-	//   std::cout << ' ' << *it;
-	// std::cout << '\n';
+	int myints[] = {16,2,77,29};
+	ft::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+	std::cout << "The contents of fifth are:";
+	for (ft::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+	  std::cout << ' ' << *it;
+	std::cout << '\n';
+
+
 	return 0;
 }
