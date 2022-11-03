b26e814f69613c90688fdfd4500710fdfcce0b6a
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-11-03 19:39:00 +0100

    vector member functions..

--- vector/main.cpp
+++ vector/main.cpp
@@ -12,6 +12,76 @@ int main ()
 	// ft::ft_vector<int> third (second.begin(),second.end());  // iterating through second
 	// ft::ft_vector<int> fourth (third);                       // a copy of third
 	ft::vector<int> fourth (second); 
+	
+	// ft::vector<int> hlwa1(3,13);
+	// ft::vector<int> hlwa2(3,37);
+	// hlwa1 = hlwa2;
+	////
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	ft::vector<int> hlwa1 (100,0);
+	ft::vector<int> hlwa2 (8,0);
+	// std::cout << "hlwa's allocator " << hlwa1.get_allocator() << std::endl;
+
+	hlwa2 = hlwa1;
+	// hlwa1 = ft::vector<int>();
+
+	std::cout << "Size of hlwa1: " << hlwa1.size() << " Capacity of hlwa1: " << int(hlwa1.capacity()) << '\n';
+	std::cout << "Size of hlwa2: " << hlwa2.size() << " Capacity of hlwa2: " << int(hlwa2.capacity()) << '\n';
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	
+	
+	
+	
+	
+	
+	//////
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	ft::vector<int> foo (3,100);   // three ints with a value of 100
+	ft::vector<int> bar (5,200);   // five ints with a value of 200
+
+	foo.swap(bar);
+
+	std::cout << "foo contains:";
+	for (unsigned i=0; i<foo.size(); i++)
+		std::cout << ' ' << foo[i];
+	std::cout << '\n';
+
+	std::cout << "bar contains:";
+	for (unsigned i=0; i<bar.size(); i++)
+		std::cout << ' ' << bar[i];
+	std::cout << '\n';
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	//////
+
+
+
+
+
+
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	ft::vector<int> empty_hlwa;
+	
+	hlwa1.clear();
+	std::cout << "hlwa is empty " << hlwa1.empty() << std::endl;
+	std::cout << "empty_hlwa is empty " << empty_hlwa.empty() << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	//////
+
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	ft::vector<int> hlwa;
+	std::cout << "hlwas size before " << hlwa.size() << std::endl;
+	hlwa.push_back(1);
+	hlwa.push_back(3);
+	hlwa.push_back(3);
+	hlwa.push_back(7);
+	hlwa.pop_back();
+	hlwa.pop_back();
+	std::cout << "hlwas size after " << hlwa.size() << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
+	std::cout << "^^^^^^^^^^^^^^" << std::endl;
 	// // the iterator constructor can also be used to construct from arrays:
 	// int myints[] = {16,2,77,29};
 	// ft::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 13:23:00 by ael-asri          #+#    #+#             */
-/*   Updated: 2022/11/02 18:27:24 by ael-asri         ###   ########.fr       */
+/*   Updated: 2022/11/03 17:12:57 by ael-asri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #define VECTOR_HPP
 
 #include <iostream>
-#include "Iterator_traits.hpp"
-#include "reverse_iterator.hpp"
+// #include "Iterator_traits.hpp"
+// #include "reverse_iterator.hpp"
 
 namespace ft
 {
@@ -33,38 +33,45 @@ namespace ft
 		// typedef	typename vector<T>::iterator		iterator;
 
 		private:
-			size_type	n;
-			size_type	len;
-			T			*v;
+			size_type		n;
+			size_type		len;
+			T				*v;
+			Allocator		_allocator;
+			// InputIterator	first;
+			// InputIterator	last;
 		public:
 			// Member functions
 				//----	Constractors
 					// default constactor
-					explicit vector (const allocator_type& alloc = allocator_type())
+					explicit vector (const allocator_type& alloc = allocator_type()) : len(0), _allocator(alloc)
 					{
-						n = 0;
+						// len = 0;
 						// (void)alloc;
 					//	v = new T[n];
-						v = alloc.allocate(n);
+						// _allocator = alloc;
+						v = _allocator.allocate(len);
+						// v = alloc;
+						// len = 0;
+						// v = new T[len];
 						std::cout << "vector default constractor called!" << std::endl;
 					};
 
 					// fill constractor
-					explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+					explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : len(n), _allocator(alloc)
 					{
-						this->n = n;
+						// this->n = n;
 						// (void)alloc;
-						 v = alloc.allocate(n);
+						 v = _allocator.allocate(len);
 						// v = new T[n];
-						for (size_type i=0; i<n; i++)
+						for (size_type i=0; i<len; i++)
 						{
 							v[i] = val;
-							std::cout << v[i] << std::endl;
+							// std::cout << v[i] << std::endl;
 						}
 						std::cout << "vector fill constractor called!" << std::endl;
 					};
 
-					// range constractor
+			/*		// range constractor
 					template <class InputIterator> vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
 					{
 						int i=0;
@@ -77,30 +84,34 @@ namespace ft
 						std::cout << "vector range constractor called!" << std::endl;
 					};
 
-					// copy constractor
-					vector (const vector& x)
+			*/		// copy constractor
+					vector (const vector& x) : len(x.len), v(x.v)
 					{
-						*this = x;
-						for (size_type i=0; i<this->n; i++)
-						{
-							std::cout << this->v[i] << std::endl;
-						}
+						// len = x.len;
+						// v = x.v;
+						// for (size_type i=0; i<len; i++)
+						// {
+						// 	std::cout << v[i] << std::endl;
+						// }
 						std::cout << "vector copy constractor called!" << std::endl;
 					};
 
 					// deconstractor
 					~vector()
 					{
+						// _allocator.dealocate();
 						std::cout << "vector Deconstractor called!" << std::endl;
 					};
 
 					// operator=
 					vector& operator=(const vector& x)
 					{
-						v = x;
+						len = x.len;
+						v = x.v;
+						return *this;
 					};
 
-				//----	Iterators
+		/*		//----	Iterators
 					// begin
 					iterator begin()
 					{
@@ -177,31 +188,33 @@ namespace ft
 						return {const_reverse_iterator::*this, len};
 					};
 
-				//----	Capacity
+		*/		//----	Capacity
 					// size
 					size_type	size() const
 					{
-						for(size_t i=0; v[i] != NULL; i++)
-							;
-						return i;
+						// for(size_t i=0; v[i] != NULL; i++)
+						// 	;
+						// return i;
+						return len;
 					};
 
-					// max_size
-					size_type	max_size() const {};
+		/*			// max_size
+					size_type	max_size() const
+					{
+						return std::distance(begin(), end());
+					};
 
 					// resize
 					void	resize(size_type n, value_type val = value_type())
 					{
-						if (n<this->n)
+						if (n<len)
 						{
-							size_type i;
-							for (i=0; i<n; i++)
-								;
-							for (i=0; i<n; i++)
+							// size_type i;
+							for (size_type i=n; i<len; i++)
 							{
 								delete v[i];
 							}
-							this->n = n;
+							len = n;
 						}
 						else
 						{
@@ -214,18 +227,21 @@ namespace ft
 						}
 					};
 
-					// capacity
-					size_type	capacity() const {};
+			*/		// capacity
+					size_type	capacity() const
+					{
+						return 2<<(len-2);
+					};
 
 					// empty
 					bool	empty() const
 					{
-						if (!v.size())
+						if (!len)
 							return true;
 						return false;
 					};
 
-					// reserve
+			/*		// reserve
 					void	reserve(size_type n)
 					{
 						if (n > v.capacity())
@@ -241,14 +257,14 @@ namespace ft
 					};
 
 				//----	Element access
-					// operator[]
-					reference	operator[](sizt_type n)
+			*/		// operator[]
+					reference	operator[](size_type n)
 					{
-						if (n<0 || n>this->n)
-							throw invalidIndex();
+						// if (n < 0 || n > len)
+						// 	throw invalidIndex();
 						return v[n];
 					};
-					const_reference	operator[](size_type n) const
+			/*		const_reference	operator[](size_type n) const
 					{
 						if (n<0 || n>this->n)
 							throw invalidIndex();
@@ -297,51 +313,76 @@ namespace ft
 					// assigne
 					template <class InputIterator>void	assigne(InputIterator first, InputIterator last) {};
 					void	assigne(size_type n, const value_type& val) {};
-					// push_back
+				*/	// push_back
 					void	push_back(const value_type& val)
 					{
-						this->insert(val, data.end());
+						// this->insert(val, data.end());
+						len+=1;
+						T *temp = v;
+						v = _allocator.allocate(len); // resize() here!
+						v = temp;
+						v[len-1] = val;
+						// for (size_type i=0; i<len; i++)
+						// 	std::cout << "hlwa's data " << v[i] << std::endl;
 					};
 					// pop_back
-					void	pop_back() {};
-					// insert
+					void	pop_back()
+					{
+						// for (size_type i=0; i<len; i++)
+						// 	std::cout << "hlwa before popin " << this->v[i] << std::endl;
+						_allocator.destroy(v+(len-1));
+						len-=1;
+						// for (size_type i=0; i<len; i++)
+						// 	std::cout << "hlwa after popin " << v[i] << std::endl;
+					};
+			/*		// insert
 					iterator	insert(iterator position, const value_type& val) {};
 					void	insert(iterator position, size_type n, const_value_type& val) {};
 					template <class InputIterator>void	insert(iterator position, InputIterator first, InputIterator last) {};
 					// erase
 					iterator	erase(iterator position) {};
 					iterator	erease(iterator first, iterator last) {};
-					// swap
-					void	swap(vector& x) 
+			*/		// swap
+					void	swap(vector& x)
 					{
-						while (x != NULL)
-						{
-							v[i] = x[i];
-							x = x->next;
-						}
+						// while (x != NULL)
+						// {
+						// 	v[i] = x[i];
+						// 	// x = x->next;
+						// 	i++;
+						// }
+						int temp_len = len;
+						T *temp_v = v;
+						len = x.len;
+						v = x.v;
+						x.len = temp_len;
+						x.v = temp_v;
 					};
 					// clear
 					void	clear()
 					{
-						while (v != NULL)
-						{
-							delete v[i];
-							v = v->next;
-						}
-						this->n = 0;
+						// while (v != NULL)
+						// for (size_type i; i<len; i++)
+						// {
+						// 	// delete (v[i]);
+						// 	// v = v->next;
+						// }
+						_allocator.destroy(v);
+						len = 0;
 					};
-					// emplace
+			/*		// emplace
 					template <class... Args>iterator emplace(const_iterator position, Args&&... args) {};
 					
 					// emplace_back
 					template <class... Args>void	emplace_back(Args&&... args) {};
 					
 
-				//----	Allocator
+			*/	//----	Allocator
 					// get_allocator
 					allocator_type	get_allocator() const
 					{
-						return this->allocate;
+						// return this->allocate;
+						return _allocator;
 					};
 	};
 }
