b1163c38ad0330cad4db7f94e9717814d0513648
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-12-12 19:18:40 +0100

    red black algorithm implementation.

--- map/main.cpp
+++ map/main.cpp
@@ -6,12 +6,12 @@
 /*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 10:11:37 by ael-asri          #+#    #+#             */
-/*   Updated: 2022/12/09 11:14:01 by ael-asri         ###   ########.fr       */
+/*   Updated: 2022/12/12 19:14:32 by ael-asri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 #include "map.hpp"
-
 int main()
 {
 	std::cout << "hello map" << std::endl;
@@ -31,4 +31,82 @@ int main()
 	// std::cout << "^^^^^^^^^^^^^^" << std::endl;
 	// ft::map<std::string, int> map3;
 	// map3 = map2;
+}
+*/
+#include "../utils/map_utils.hpp"
+/*
+int main()
+{
+    bst<int> b;
+	bst<int> *root;
+    root = b.insert(root, 50);
+    b.insert(root, 30);
+    b.insert(root, 20);
+    b.insert(root, 40);
+    b.insert(root, 70);
+    b.insert(root, 60);
+    b.insert(root, 80);
+ 
+    b.inorder(root);
+    return 0;
+}
+int main()
+{
+ */   /* Let us create following BST
+              50
+            /    \
+          30     70
+         /  \   /  \
+       20   40 60  80 */
+ /*   node<int>	n;
+	node<int>	*root;
+    root = n.insert_node(root, 50);
+    n.insert_node(root, 30);
+    n.insert_node(root, 20);
+    n.insert_node(root, 40);
+    n.insert_node(root, 70);
+    n.insert_node(root, 60);
+    n.insert_node(root, 80);
+ 
+    printf("Inorder traversal of the given tree \n");
+    n.inorder(root);
+ 
+    printf("\n\nDelete 20\n");
+    root = n.delete_node(root, 20);
+    printf("Inorder traversal of the modified tree \n");
+    n.inorder(root);
+ 
+    printf("\n\nDelete 30\n");
+    root = n.delete_node(root, 30);
+    printf("Inorder traversal of the modified tree \n");
+    n.inorder(root);
+ 
+    printf("\n\nDelete 50\n");
+    root = n.delete_node(root, 50);
+    printf("Inorder traversal of the modified tree \n");
+    n.inorder(root);
+ 
+    return 0;
+}
+*/
+
+int main()
+{
+    RBtree<int> tree;
+ 
+    tree.insert(7);
+    tree.insert(6);
+    tree.insert(5);
+    tree.insert(4);
+    tree.insert(3);
+    tree.insert(2);
+    tree.insert(1);
+ 
+    std::cout << "Inorder Traversal of Created Tree\n";
+    tree.inorder();
+ 
+    // std::cout << "\n\nLevel Order Traversal of Created Tree\n";
+    // tree.levelOrder();
+ 
+    return 0;
 }
--- utils/map_utils.hpp
+++ utils/map_utils.hpp
@@ -6,16 +6,136 @@
 /*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 13:20:30 by ael-asri          #+#    #+#             */
-/*   Updated: 2022/12/09 11:48:31 by ael-asri         ###   ########.fr       */
+/*   Updated: 2022/12/12 19:14:21 by ael-asri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_UTILS_HPP
 #define MAP_UTILS_HPP
 
-template <typename T> class node
+#include <iostream>
+/*
+
+
+template <typename T> RB_Tree()
 {
-	T		value;
+
+};
+template<typename T> class bst
+{
+	public:
+		T	data;
+		bst	*parent;
+		bst	*right;
+		bst	*left;
+		int	color;
+		
+		bst() : data(0), parent(0), right(0), left(0), color(0) {};
+		bst( T value ) : data(value), parent(0), right(0), left(0), color(0) {};
+		~bst() {};
+
+		
+};
+*/
+
+
+/*
+template<typename T> class node
+{
+	public:
+		T		data;
+		node	*parent;
+		node	*right;
+		node	*left;
+		int		color;
+
+		node *new_node( T value )
+		{
+			node *n = new node<T>;
+
+			n->data = value;
+			n->parent = n->right = n->left = NULL;
+			n->color = 0;
+			return n;
+		};
+
+		node *insert_node( node *root, T value )
+		{
+			if (!root)
+				return new_node(value);
+			if (value > root->data)
+				root->right = insert_node(root->right, value);
+			else if (value < root->data)
+				root->left = insert_node(root->left, value);
+			root->color = 0;
+			std::cout << "root color " << root->color << std::endl;
+			return root;
+		};
+
+		node *minval_node( node *root )
+		{
+			node *current = root;
+
+			while (current && current->left != NULL)
+				current = current->left;
+			return current;
+		};
+
+		node *maxval_node( node *root )
+		{
+			node *current = root;
+
+			while (current && current->right != NULL)
+				current = current->right;
+			return current;
+		};
+
+		node *delete_node( node *root, T value )
+		{
+			if (!root)
+				return root;
+			if (value > root->data)
+				root->right = delete_node(root->right, value);
+			else if (value < root->data)
+				root->left = delete_node(root->left, value);
+			else
+			{
+				if (!root->right && !root->left)
+					return NULL;
+				else if (!root->right)
+				{
+					struct node *temp = root->left;
+					free(root);
+					return temp;
+				}
+				else if (!root->left)
+				{
+					struct node *temp = root->right;
+					free(root);
+					return temp;
+				}
+				struct node *temp = minval_node(root->right);
+				root->data = temp->data;
+				root->right = delete_node(root->right, temp->data);
+			}
+			return root;
+		};
+
+		void	inorder( node* root )
+		{
+			if (!root)
+				return;
+			inorder(root->left);
+			std::cout << root->data << std::endl;
+			inorder(root->right);
+		}
+};
+*/
+
+/*
+template <typename T> struct node
+{
+	T		data;
 	node	*parent;
 	node	*right;
 	node	*left;
@@ -25,10 +145,68 @@ template <typename T> class node
 	// node( T &n ) : value(p), parent(0), left(0), right(0) {};
 };
 
-template <typename T> RB_Tree()
+template<typename T> node *new_node( T value )
 {
+	node *n = new node<T>;
 
+	n->data = value;
+	n->parent = n->right = n->left = NULL;
+	n->color = 0;
+	return n;
 };
+
+template<typename T> node *insert_node( node *root<T>, T value )
+{
+	if (!root)
+		return new_node(value);
+	if (value > root->data)
+		root->right = insert(root->right, value);
+	else if (value < root->data)
+		root->left = insert(root->left, value);
+	root->color = 0;
+	std::cout << "root color " << root->color << std::endl;
+	return root;
+};
+
+template<typename T> node *delete_node( node *root, T value )
+{
+	if (!root)
+		return root;
+	if (key > root->data)
+		root->right = delete_node(root->right, value);
+	else if (key < root->data)
+		root->left = delete_node(root->left, value);
+	else
+	{
+		if (!root->right && !root->left)
+			return NULL;
+		else if (!root->right)
+		{
+			struct node *temp = root->left;
+			free(root);
+			return temp;
+		}
+		else if (!root->left)
+		{
+			struct node *temp = root->right;
+			free(root);
+			return temp;
+		}
+		struct node *temp = minval_node(root->right);
+		root->data = temp->data;
+		root->right = delete_node(root->right, temp->data);
+	}
+	return root;
+};
+
+void	inorder( node* root )
+{
+	if (!root)
+		return;
+	inorder(root->left);
+	std::cout << root->data << std::endl;
+	inorder(root->right);
+}*/
 /*
 // #include "../map/map.hpp"
 #include "map_Iterators.hpp"
@@ -74,5 +252,188 @@ int	key_exists(map m, Key key)
 	return 0;
 }
 */
+
+////////////////////////////////////////////////////
+
+enum Color { RED, BLACK };
+
+template<typename T> struct node
+{
+	T		data;
+	bool	color;
+	node	*parent, *right, *left;
+
+	node( T value )
+	{
+		data = value;
+		parent = right = left = NULL;
+		color = RED;
+	};
+};
+
+template<typename T> class RBtree
+{
+	private:
+		node<T>	*root;
+	protected:
+		void	rotateRight( node<T> *&, node<T> *& );
+		void	rotateLeft( node<T> *&, node<T> *& );
+		void	fixViolation( node<T> *&, node<T> *& );
+	public:
+		RBtree() { root = NULL; };
+		void	insert( const T &n );
+		void	inorder();
+		// void	levelOrder();
+};
+
+template<typename T> node<T> *insert_bst( node<T> *root, node<T> *n )
+{
+	if (!root)
+		return n;
+	if (n->data > root->data)
+	{
+		root->right = insert_bst(root->right, n);
+		root->right->parent = root;
+	}
+	else if (n->data < root->data)
+	{
+		root->left = insert_bst(root->left, n);
+		root->left->parent = root;
+	}
+	return root;
+};
+
+template<typename T> void inorderHelper( node<T> *root )
+{
+	if (!root)
+		return;
+	inorderHelper(root->left);
+	std::cout << root->data << std::endl;
+	inorderHelper(root->right);
+}
+
+
+template<typename T> void	RBtree<T>::rotateLeft( node<T> *&root, node<T> *&n )
+{
+	node<T> *temp = n->right;
+
+	n->right = temp->left;
+	if (n->right != NULL)
+		n->right->parent = n;
+	temp->parent = n->parent;
+	if (n->parent == NULL)
+		root = n->right;
+	else if (n == n->parent->left)
+		n->parent->left = temp;
+	else
+		n->parent->right = temp;
+	temp->left = n;
+	n->parent = temp;
+};
+
+template<typename T> void	RBtree<T>::rotateRight( node<T> *&root, node<T> *&n )
+{
+	node<T> *temp = n->left;
+
+	n->left = temp->right;
+	if (n->left != NULL)
+		n->left->parent = n;
+	temp->parent = n->parent;
+	if (n->parent == NULL)
+		root = temp;
+	else if (n == n->parent->left)
+		n->parent->left = temp;
+	else
+		n->parent->right = temp;
+	temp->right = n;
+	n->parent = temp;
+};
+
+template<typename T> void	RBtree<T>::fixViolation( node<T> *&root, node<T> *&n )
+{
+	node<T> *temp_parent = NULL, *temp_gparent = NULL, *temp_uncle;
+
+	while (n != root && n->color != BLACK && n->parent->color == RED)
+	{
+		temp_parent = n->parent;
+		temp_gparent = n->parent->parent;
+		
+		// case-1: parent of n is the left child of grand-parent of n
+		if (temp_parent == temp_gparent->left)
+		{
+			temp_uncle = temp_gparent->right;
+			
+			// case-1.1: the uncle of n is also red so only recoloring required
+			if (temp_uncle && temp_uncle->color == RED)
+			{
+				temp_gparent->color = RED;
+				temp_parent->color = BLACK;
+				temp_uncle->color = BLACK;
+				n = temp_gparent;
+			}
+			else
+			{
+				// case-1.2: n is the right child of its parent so leftRotation required
+				if (n == temp_parent->right)
+				{
+					rotateLeft(root, temp_parent);
+					n = temp_parent;
+					temp_parent = n->parent;
+				}
+				// case-1.3: n is the left child of its parent so rightRotation required
+				rotateRight(root, temp_gparent);
+				std::swap(temp_parent->color, temp_gparent->color);
+				n = temp_parent;
+			}
+		}
+		// case-2: parent of n is the right child of grand-parent of n
+		else
+		{
+			temp_uncle = temp_gparent->left;
+
+			// case-2.1: the uncle of n is also red so only recoloring required
+			if (temp_uncle != NULL && temp_uncle->color == RED)
+			{
+				temp_gparent->color = RED;
+				temp_parent->color = BLACK;
+				temp_uncle->color = BLACK;
+				n = temp_gparent;
+			}
+			else
+			{
+				// case-2.2: n is the left child of its parent so rightRotation required
+				if (n == temp_parent->left)
+				{
+					rotateRight(root, temp_parent);
+					n = temp_parent;
+					temp_parent = n->parent;
+				}
+				// case-2.3: n is the right child of its parent so leftRotation required
+				rotateLeft(root, temp_gparent);
+				std::swap(temp_parent->color, temp_gparent->color);
+				n = temp_parent;
+			}
+		}
+	}
+	root->color = BLACK;
+};
+
+template<typename T> void	RBtree<T>::insert( const T &value )
+{
+	node<T> *n = new node<T>(value);
+
+	root = insert_bst(root, n);
+	fixViolation(root, n);
+};
+
+template<typename T> void	RBtree<T>::inorder( void )
+{
+	inorderHelper(root);
+}
+
+// template<typename T> void	RBtree<T>::levelOrder( void )
+// {
+// 	levelOrderHelper(root);
+// }
 
 #endif
