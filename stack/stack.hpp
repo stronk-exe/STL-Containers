a164e4e906da482ac78afa4e0272afa04f927a0e
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-12-25 18:47:40 +0100

    stack `-`

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -24,68 +24,68 @@ namespace ft
 
         
         public:
-            typedef T           value_type;
-            typedef size_t      size_type;
-            typedef Container   container_type;
+            typedef T                                   value_type;
+            typedef Container                           container_type;
+            typedef typename Container::size_type       size_type;
             // typedef &value_type          reference;
             // typedef const &value_type          const_reference;
             // const_reference	Container::const_reference;
+        protected:
+            container_type	s;
+        
+        public:
             // >> Member functions
 				//----	Constructors
-                    explicit stack( const Container& cont = Container() ) : c(cont) {};
-                    // stack( const stack& other ) : c(other.c) {};
+                    explicit stack( const Container& cont = Container() ) : s(cont) {};
+                    stack( const stack& other ) : s(other.s) {};
 
                 //----  Destructor
                     ~stack(){};
 
                 //----  operator=
-                    // stack& operator=( const stack& other )
-                    // {
-
-                    // };
+                    stack& operator=( const stack& other )
+                    {
+                        if (*this != other)
+                            s = other.s;
+                        return *this;
+                    };
 
                 //----  Element access
                     // top
                         value_type& top()
                         {
-                            return c.back();
+                            return s.back();
                         };
                         const value_type& top() const
                         {
-                            return c.back();
+                            return s.back();
                         };
                 
                 //----  capacity
                     // empty
                         bool empty() const
                         {
-                            return c.empty();
+                            return s.empty();
                         };
                     
                     // size
                         size_type size() const
                         {
-                            return c.size();
+                            return s.size();
                         };
                 
                 //----  modifiers
                     // push
                         void push( const value_type& value )
                         {
-                            c.push_back(value);
+                            s.push_back(value);
                         };
-                        // void push( value_type&& value )
-                        // {
-                        //     c.pop_back(value);
-                        // };
 
                     // pop
                         void pop()
                         {
-                            c.pop_back();
+                            s.pop_back();
                         };
-        protected:
-            container_type	c;
     };
             //>> Non member functions
                 template< class T, class Container > bool operator==( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
