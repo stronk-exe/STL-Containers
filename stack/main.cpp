c9a44a6443cf41de29ae269243e5617ce6fa00f4
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-12-28 13:29:51 +0100

    stack 👍

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,18 +1,299 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 11:45:37 by ael-asri          #+#    #+#             */
+/*   Updated: 2022/12/28 12:02:10 by ael-asri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "stack.hpp"
+
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
+
+
+#define EQUAL(x) ((x) ? (std::cout << "\033[1;32mOK\033[0m\n") : (std::cout << "\033[1;31mKO\033[0m\n"))
+
+void	stackConstructors(void)
+{
+    std::cout <<"	default constructor ---	";
+	{
+		std::vector<int> myvector (2,200);
+		ft::stack<int,std::vector<int> > s(myvector);
+		EQUAL(s.size() == 2);
+	}
+}
+
+void	stackMemberFunctions(void)
+{
+    std::cout << "	empty method ----	";
+	{
+		ft::stack<int> mystack;
+		int sum (0);
+
+		for (int i=1;i<=10;i++) mystack.push(i);
+
+		while (!mystack.empty())
+		{
+			sum += mystack.top();
+			mystack.pop();
+		}
+		EQUAL(sum == 55);
+	}
+    std::cout << "	size method ----	";
+	{
+		bool cond(false);
+		ft::stack<int> myints;
+		cond = myints.size() == 0;
+
+		for (int i=0; i<5; i++) myints.push(i);
+		cond = cond && (myints.size() == 5);
+
+		myints.pop();
+		cond = cond && (myints.size() == 4);
+		EQUAL(cond);
+	}
+    std::cout << "	top method ----		";
+	{
+		ft::stack<int> mystack;
+
+		mystack.push(10);
+		mystack.push(20);
+
+		mystack.top() -= 5;
+		std::vector<int> vec(10, 50);
+		vec.push_back(300);
+		ft::stack<int, std::vector<int> > const c_mystack(vec);
+
+		EQUAL(mystack.top() == 15 && c_mystack.top() == 300);
+	}
+    std::cout << "	push & pop methods ----	";
+	{
+		int sum(0);
+		ft::stack<int> mystack;
+
+		for (int i=0; i<5; ++i) mystack.push(i);
+
+		while (!mystack.empty())
+		{
+			sum += mystack.top();
+			mystack.pop();
+		}
+		EQUAL(sum == 10);
+	}
+}
+
+void	stackRelationalOperators(void)
+{
+    std::cout << "	operator== ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack ==  mystack1) == (stack == stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack ==  mystack1) == (stack == stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack ==  mystack1) == (stack == stack1);
+		}
+		EQUAL(cond);
+	}
+	std::cout << "	operator!= ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack !=  mystack1) == (stack != stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack !=  mystack1) == (stack != stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack !=  mystack1) == (stack != stack1);
+		}
+		EQUAL(cond);
+	}
+	std::cout << "	operator< ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack < mystack1) == (stack < stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack < mystack1) == (stack < stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack < mystack1) == (stack < stack1);
+		}
+		EQUAL(cond);
+	}
+    std::cout << "	operator<= ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack <= mystack1) == (stack <= stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack <= mystack1) == (stack <= stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack <= mystack1) == (stack <= stack1);
+		}
+		EQUAL(cond);
+	}
+	std::cout << "	operator> ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack > mystack1) == (stack > stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack > mystack1) == (stack > stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack > mystack1) == (stack > stack1);
+		}
+		EQUAL(cond);
+	}
+	std::cout << "	operator>= ----		";
+	{
+		bool cond(false);
+		std::vector<int>	vec(100, 2);
+		std::vector<int>	vec1(100, 2);
+		{
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = (mystack >= mystack1) == (stack >= stack1);
+		}
+		{
+			vec.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack >= mystack1) == (stack >= stack1);
+		}
+		{
+			vec.pop_back();
+			vec1.push_back(300);
+			ft::stack<int, std::vector<int> > mystack1(vec);
+			ft::stack<int, std::vector<int> > mystack(vec1);
+			std::stack<int, std::vector<int> > stack1(vec);
+			std::stack<int, std::vector<int> > stack(vec1);
+			cond = cond && (mystack >= mystack1) == (stack >= stack1);
+		}
+		EQUAL(cond);
+	}
+}
 
 int main()
 {
-    std::cout << "hello mr stack" << std::endl;
-    ft::stack<int> c1;
-    c1.push(5);
-    c1.push(7);
-    c1.push(7);
-    std::cout << c1.size() << '\n';
- 
-    ft::stack<int> c2(c1);
-    std::cout << c2.size() << '\n';
- 
-    // std::deque<int> deq (3,5);
-    // ft::stack<int> c3(deq); // overload (2)
-    // std::cout << c3.size() << '\n';
+    std::cout << "* Stack Constructors;" << std::endl;
+    stackConstructors();
+	std::cout << std::endl;
+
+    std::cout << "* Stack Member Functions" <<  std::endl;
+    stackMemberFunctions();
+	std::cout << std::endl;
+
+    std::cout << "* Stack Relational Operators" << std::endl;
+    stackRelationalOperators();
+	return 0;
 }
--- stack/stack.hpp
+++ stack/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:12:15 by ael-asri          #+#    #+#             */
-/*   Updated: 2022/11/02 17:12:18 by ael-asri         ###   ########.fr       */
+/*   Updated: 2022/12/28 13:22:44 by ael-asri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,99 +18,120 @@
 
 namespace ft
 {
-    template<class T, class Container = ft::vector<T> > class stack
-    {
-        // private:
-
-        
+	template<class T, class Container = ft::vector<T> > class	stack
+	{
         public:
-            typedef T                                   value_type;
-            typedef Container                           container_type;
-            typedef typename Container::size_type       size_type;
-            // typedef &value_type          reference;
-            // typedef const &value_type          const_reference;
-            // const_reference	Container::const_reference;
-        protected:
+            typedef T								value_type;
+            typedef Container						container_type;
+            typedef typename Container::size_type	size_type;
+
+		protected:
             container_type	s;
         
-        public:
-            // >> Member functions
+		public:
+			// >> Member functions
 				//----	Constructors
-                    explicit stack( const Container& cont = Container() ) : s(cont) {};
-                    stack( const stack& other ) : s(other.s) {};
+					explicit stack( const Container& cont = Container() ) : s(cont) {};
+					stack( const stack& other ) : s(other.s) {};
 
-                //----  Destructor
-                    ~stack(){};
+				//----  Destructor
+					~stack(){};
 
                 //----  operator=
-                    stack& operator=( const stack& other )
+					stack& operator=( const stack& other )
                     {
-                        if (*this != other)
-                            s = other.s;
-                        return *this;
-                    };
+						if (*this != other)
+							s = other.s;
+						return *this;
+					};
 
-                //----  Element access
-                    // top
-                        value_type& top()
-                        {
-                            return s.back();
-                        };
-                        const value_type& top() const
-                        {
-                            return s.back();
-                        };
-                
-                //----  capacity
-                    // empty
-                        bool empty() const
-                        {
-                            return s.empty();
-                        };
-                    
-                    // size
-                        size_type size() const
-                        {
-                            return s.size();
-                        };
-                
-                //----  modifiers
-                    // push
-                        void push( const value_type& value )
-                        {
-                            s.push_back(value);
-                        };
+				//----  Element access
+					// top
+						value_type& top()
+						{
+							return s.back();
+						};
+						const value_type& top() const
+						{
+							return s.back();
+						};
 
-                    // pop
-                        void pop()
-                        {
-                            s.pop_back();
-                        };
+				//----  capacity
+					// empty
+						bool empty() const
+						{
+							return s.empty();
+						};
+
+					// size
+						size_type size() const
+						{
+							return s.size();
+						};
+
+				//----  modifiers
+					// push
+						void push( const value_type& value )
+						{
+							s.push_back(value);
+						};
+
+					// pop
+						void pop()
+						{
+							s.pop_back();
+						};
+
+                // Relational operators
+                    friend bool operator==( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s == rhs.s;
+                    // };
+                    friend bool operator!=( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s != rhs.s;
+                    // };
+                    friend bool operator<( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s < rhs.s;
+                    // };
+                    friend bool operator<=( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s <= rhs.s;
+                    // };
+                    friend bool operator>( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s > rhs.s;
+                    // };
+                    friend bool operator>=( const stack<T,Container>&, const stack<T,Container>& );
+                    // {
+                    //     return lhs.s >= rhs.s;
+                    // };
     };
             //>> Non member functions
-                template< class T, class Container > bool operator==( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator==( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c == rhs.c;
+                    return lhs.s == rhs.s;
                 };
-                template< class T, class Container > bool operator!=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator!=( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c != rhs.c;
+                    return lhs.s != rhs.s;
                 };
-                template< class T, class Container > bool operator<( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator<( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c < rhs.c;
+                    return lhs.s < rhs.s;
                 };
-                template< class T, class Container > bool operator<=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator<=( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c <= rhs.c;
+                    return lhs.s <= rhs.s;
                 };
-                template< class T, class Container > bool operator>( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator>( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c > rhs.c;
+                    return lhs.s > rhs.s;
                 };
-                template< class T, class Container > bool operator>=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs )
+                template< class T, class Container > bool operator>=( const stack<T,Container>& lhs, const stack<T,Container>& rhs )
                 {
-                    return lhs.c <= rhs.c;
+                    return lhs.s <= rhs.s;
                 };
                 // template< class T, class Container > bool operator!=( const std::stack<T,Container>& lhs, const std::stack<T,Container>& rhs )
                 // {
